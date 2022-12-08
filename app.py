5a50db3bdeb9193192ad821945cf0a0ff5dd9b4c
angel <angel.escobedo03@utrgv.edu>
2022-12-08 10:17:12 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+#integrate our library and objects 
+from flask import Flask, render_template
+
+#create our flask instance/application 
+app = Flask(__name__)
+#create our directory 
+@app.route('/')
+def home2():
+  return render_template("index.html")
+
+@app.route('/')
+def about():
+  return render_template('index.html')
+
+#run our aplication
+if __name__ == "__main__":
+  app.run(debug=True)
